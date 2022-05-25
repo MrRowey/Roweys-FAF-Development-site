ff607eb4e6b54ea8786187037952226ab360e26c
MrRowey <row.josh02@gmail.com>
2022-05-25 23:32:09 +0100

    adding maps
    - Adding maps

--- data/maps.php
+++ data/maps.php
@@ -1,23 +1,52 @@
 <?php
 include('../dbconfig.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(empty($_POST['player'])) {
-        $nameErr = "Username is Required";
-    }
-    else {
-        $player = mysqli_real_escape_string($link,$_REQUEST['player']);
+$targetDir = "images/maps/";
+$fileName = basename($_FILES["file"]["name"]);
+$targetFilePath = $targetDir . $fileName;
+$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
 
-        $sql = "INSERT INTO players (name) VALUES ('$player')";
-        if(mysqli_query($link, $sql)){
-        echo "Record Added";
-        header("location: ../index.php");
-        } else {
-        echo "Error: Could not execute $sql. " . mysqli_error($link);
+if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
+    // Allow certain file formats
+    $allowTypes = array('jpg','png','jpeg','gif','pdf');
+    if(in_array($fileType, $allowTypes)){
+        // Upload file to server
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            // Insert image file name into database
+            $insert = $conn->query("INSERT into maps (fileName) VALUE ('".$fileName."', NOW())");
+            if($insert){
+                $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+            }else{
+                $statusMsg = "File upload failed, please try again.";
+            } 
+        }else{
+            $statusMsg = "Sorry, there was an error uploading your file.";
         }
+    }else{
+        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+    }
+}else{
+    $statusMsg = 'Please select a file to upload.';
+}
+
+
+if(empty($_POST['mapName'])) {
+    $nameErr = "Map Details Are Required";
+}
+else {
+    $mapName = mysqli_real_escape_string($conn,$_REQUEST['mapName']);
+    $mapSize = mysqli_real_escape_string($conn,$_REQUEST['mapSize']);
+    $sql = "INSERT INTO maps (Name,Size) VALUES ('$mapName',$mapSize)";
+    if(mysqli_query($conn, $sql)){
+    echo "Record Added";
+    header("location: ../index.php");
+    } else {
+    echo "Error: Could not execute $sql. " . mysqli_error($conn);
     }
 }
 
+// Display status message
+echo $statusMsg;
 ?>
 <html>
 <title>FAF Tournament Match Data</title>
@@ -37,14 +66,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <h1 class="player-h1">Adding Map</h1>
     <p class="player-p">Please Add the Name of the Map you need to add to the data base </p>
 
-    <form class="add" action="/add/add_players.php" method="post">
+    <form class="w3-container" action="/data/maps.php" method="post">
         <label>Map Name</label> 
         <input type="text" name="mapName" id="mapName"><br><br>
         <label>Map Size</label> 
         <input type="number" name="mapSize" id="mapSize"><br><br>
         <label>Upload Map Image</label> 
-        <input type="text" name="mapSize" id="mapNameplayer"><br><br>
-        <input type="submit" value="Submit">
+        <input type="file" name="file" id="mapImg"><br><br>
+        <input class="w3-button w3-green" type="submit" value="Submit">
 
     </form> 
 </main>
