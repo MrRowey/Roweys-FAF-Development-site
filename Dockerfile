1e1f402f91589e3d3b0b77a400ea6f658b6f6205
Rowey <row.josh02@gmail.com>
2022-07-05 12:42:04 +0100

    File Orgnisation, Folder Restructer

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 ##Build Docekr Image
-FROM ubuntu
-RUN apt update
-RUN apt install -y apache2
-RUN apt install -y apache2-utils
-RUN apt install -y php php-mysql
-RUN apt clean
-RUN git clone https://github.com/MrRowey/Roweys-FAF-Development-site.git
-COPY src/ src
-WORKDIR /var/www/html
-EXPOSE 80
+##FROM ubuntu
+##RUN apt update
+##RUN apt install -y apache2
+##RUN apt install -y apache2-utils
+##RUN apt install -y php php-mysql
+##RUN apt clean
+##RUN git clone https://github.com/MrRowey/Roweys-FAF-Development-site.git
+##COPY src/ src
+##WORKDIR /var/www/html
+##EXPOSE 80
--- index.php
+++ index.php
@@ -1,35 +1,63 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Rowey's FAF Development site</title>
+    <title>FAF Tournament Match Data</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <script src="https://kit.fontawesome.com/ef83ee7b4b.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <linl rel="stylesheet" href="../routes/nav.css">
     <style>
-        body,h1 {font-family: "Raleway",sans-serif;}
-        body,html {height: 100%;}
-        .bgimg {
-            background-image: url('/images/commingsoon.jpg');
-            min-height: 100%;
-            background-position: center;
-            background-size: cover;
-        }
+        /* Light Theme  */
+        .w3-theme-main-light {color:#fff !important; background-color:#070928 !important}
+        .w3-theme-acc1-light {background-color:#6C72CB !important}
+        .w3-theme-acc2-light {background-color:#CD69C1 !important}
+        
+
+        /* Dark Theme  */
+        .w3-theme-main-dark {color:#EEEDF0 !important; background-color:#17181f !important}
+        .w3-theme-acc1-dark {background-color:#3456FF !important}
+        .w3-theme-acc2-dark {background-color:#F8F8FA !important}
+
     </style>
+
+
+
+
+
 </head>
-<main class="bgimg w3-display-container w3-animate-opacity w3-text-white">
-    <div class="w3-display-topleft w3-padding-large w3-xlarge">
-        Rowey's FAF Development site
-    </div>
-    <div class="w3-display-middle">
-        <h1 class="w3-jumbo w3-animate-top">Comming Soon</h1>
-        <hr class="w3-border-grey" style="margin:auto;width:40%">
-        <a href="../FAF-Tourney-Data-DB/public/Tournament-Data-DB/TounamentLandingPage.php" type="button">HomePage</a>
-        <p class="w3-large w3-center">Development in Progress</p>
-    </div>
-    <div class="w3-display-bottomleft w3-padding-large">
-        Powerd By <i class="fa-brands fa-github"></i> Github Repository & Heroku
-    </div>
+<nav class="w3-bar w3-blue">
+    <a href="/index.php" class="w3-bar-item w3-button w3-mobile">Home</a>
+    <a href="/pages/1v1.php" class="w3-bar-item w3-button w3-mobile">1v1 Match Data</a>
+    <a href="/pages/AddData.php" class="w3-bar-item w3-button w3-mobile">Add Data</a>
+    <!--<a href="#" class="w3-bar-item w3-button w3-mobile">Link 2</a>-->
+    <a href="/login.php" class="w3-bar-item w3-button w3-mobile" style="float: right;" >Login</a>
+</nav>
+
+<label for="theme" class="theme">
+    <span>Light</span>
+    <span class="theme__toggle-wrap">
+        <input id="theme" class="theme__toggle" type="checkbox" role="switch" name="theme" value="dark">
+        <span class="theme__fill"></span>
+        <span class="theme__icon">
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+            <span class="theme__icon-part"></span>
+        </span>
+    </span>
+    <span>Dark</span>
+</label>
+
+
+<main >
+    <h1> FAF Tournament Match Data</h1>
+    <p> This site is to display and look up Tournament data. Currenly 1v1 Data is only mesured at anything with a min of $50 Prize pool</p>
+    <img src="/images/1.jpg" alt="FAF Image">
 </main>
 </html>
