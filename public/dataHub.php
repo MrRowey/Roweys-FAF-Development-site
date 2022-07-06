8478e01b1997492b904850e1b7807603513e27f1
Rowey <row.josh02@gmail.com>
2022-07-06 21:05:43 +0100

    Replace  w3.css with development tailwindcss CDN

--- public/dataHub.php
+++ public/dataHub.php
@@ -10,12 +10,17 @@
 </head>
 <nav class="w3-bar w3-blue">
     <a href="/index.php" class="w3-bar-item w3-button w3-mobile">Home</a>
-    <a href="/public/tournamentMatchData.php" class="w3-bar-item w3-button w3-mobile">Tournament Match Data</a>
+    <a href="/public/tournamentMatchData.php" class="w3-bar-item w3-button w3-mobile">DataHub</a>
     <a href="/login.php" class="w3-bar-item w3-button w3-mobile" style="float: right;" >Login</a>
 </nav>
 
 <main >
     <h1> FAF Tournament Match Data</h1>
     <p> This site is to display and look up Tournament data. Currenly 1v1 Data is only mesured at anything with a min of $50 Prize pool</p>
+
+    <a href="../public/tournamentMatchData.php" type="w3-button">Tournament Match Data</a>
+    <a href="../public/1v1MatchData.php" type="w3-button">Total 1v1 Match Data</a>
+    <a href="../public/playerMatchData.php" type="w3-button">Player Data</a> 
+
 </main>
 </html>
