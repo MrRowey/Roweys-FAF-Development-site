6c742e4c187926e4e97fe704d5db4efeb56eb940
Rowey <row.josh02@gmail.com>
2022-07-06 23:31:05 +0100

    footer

--- index.php
+++ index.php
@@ -32,8 +32,8 @@
     </div>
   </div>
 </header>
-
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column text-center text-white bg-dark">
+<div class="d-flex h-100 text-center text-white bg-dark">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <main class="px-3">
     <h1>Cover your page.</h1>
     <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
@@ -48,5 +48,6 @@
       <span class="text-muted">Placeholder</span>
     </div>
   </footer>
+</div>
 </div>
 </html>
