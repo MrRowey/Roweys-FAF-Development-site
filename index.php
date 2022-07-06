9849a0b47490c1561dfd8942636734aaa4f01b45
Rowey <row.josh02@gmail.com>
2022-07-06 23:18:51 +0100

    123

--- index.php
+++ index.php
@@ -34,14 +34,38 @@
 </header>
 
 <div class="d-flex h-100 text-center text-white bg-dark">
-<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-  <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-    <p class="lead">
-      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
-    </p>
-  </main>
-</div>
+  <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/index.php" class="nav-link px-2 text-secondary active">Home</a></li>
+          <li><a href="/public/1v1MatchData.php" class="nav-link px-2 text-white">1v1 Data</a></li>
+          <li><a href="/public/playerMatchData.php" class="nav-link px-2 text-white">Player Data</a></li>
+          <li><a href="/public/tournamentMatchData.php" class="nav-link px-2 text-white">Tournament Data</a></li>
+          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+        </ul>
+
+
+        <div class="text-end justi">
+          <a type="button" class="btn btn-outline-light me-2" href="/Login/login.php">Login</a>
+          <a type="button" class="btn btn-warning">Sign-up</a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <main class="px-3">
+      <h1>Cover your page.</h1>
+      <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+      <p class="lead">
+        <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+      </p>
+    </main>
+  </div>
 </div>
 </html>
